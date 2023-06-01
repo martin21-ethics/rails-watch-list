9cc6fa68a078efd3fc8cbc86922a2e64818e93e5
martin hoenle <martin.hoenle@icloud.com>
2023-06-01 04:39:47 +0200

    Routing, Controller, Views for Lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,36 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show]
+
+  def index
+    @lists = List.all
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: "List was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
